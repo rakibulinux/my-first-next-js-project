a665a6ed9778ca8a997873ddfcd5b8000338c1f9
Rakib Linux <rakibulinux@gmail.com>
2022-12-26 18:16:26 +0600

    my first next project

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const index = () => {
+  return (
+    <div>
+      <h1>This is blog 2 s</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default index;
---
+++ pages/users/index.js
@@ -0,0 +1,16 @@
+const Users = ({ posts }) => {
+  console.log(posts);
+  return <div>We have {posts.length} Users</div>;
+};
+
+export default Users;
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
